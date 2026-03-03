2527615e51fc9343217e6345a95096d8dcc9d694
Arturo (Bot) <arturo@gnailuy.com>
2026-03-02 18:53:10 -0800

    Revert "chore: allow dev origins via env"
    This reverts commit 8b3f4a79182ad95fcf56e2aeca4474b69508630b.

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: process.env.NEXT_ALLOWED_DEV_ORIGINS
-      ? process.env.NEXT_ALLOWED_DEV_ORIGINS.split(",")
-      : [],
-  },
+  /* config options here */
 };
 
 export default withNextIntl(nextConfig);
